f359eee907323f0953a3476afafcccb754381598
TotalEpicness <42422230+PhantomEpicness@users.noreply.github.com>
2019-11-25 16:32:03 -1000

    Update wew.py

--- wew.py
+++ wew.py
@@ -1,12 +1,12 @@
 from twilio.rest import Client
 import time
 def sendText(userInput="i feel empty"):
-	accountSSID = 'ACe5c7e8602f7093eb54ec24f0a2da25ff'
-	authToken = '0e5385500edf90d482a28fb7a35bc66e'
+	accountSSID = ''
+	authToken = ''
 	
 	twilioCli = Client(accountSSID, authToken)
-	myTwilioNumber = '+15189074216'
-	myCellPhone = '+18085907487'
+	myTwilioNumber = ''
+	myCellPhone = ''
 	
 	message = twilioCli.messages.create(
 		body='my python app says '+userInput,
